efa5a3699046c6f9056502e087ddbae0779196d9
Carlos Mariano Belmonte <cbelmonte.laboral@gmail.com>
2022-07-30 23:27:07 -0300

    Se agrega el Archivo productos.txt al desafio

--- desafio_Archivos/index.js
+++ desafio_Archivos/index.js
@@ -13,11 +13,11 @@ class Contenedor{
         myObj.thumbnail = thumbnail;
         this.countId = this.countId+1;
         myObj.id = this.countId   
-        return myObj.id        
+        return `${myObj.id} ${this.fileData}`       
     }
 }
 
-let productos = new Contenedor('productos.txt');
+let productos = new Contenedor('./productos.txt');
 console.log(productos.save({title: 'Escuadra',price: 123.45,thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png'}))
 console.log(productos.save({title: 'Calculadora',price: 234.56,thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/calculator-math-tool-school-256.png'}))
 console.log(productos.save({title: 'Globo Terráqueo',price: 345.67,thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/globe-earth-geograhy-planet-school-256.png'}))
