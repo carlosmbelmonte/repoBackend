61c35eaa6254612428f438a07f7b0b3940412911
Carlos Mariano Belmonte <cbelmonte.laboral@gmail.com>
2022-07-31 12:33:00 -0300

    Se agrega la funcion save() al desafio

--- desafio_Archivos/index.js
+++ desafio_Archivos/index.js
@@ -1,23 +1,48 @@
+const fs = require('fs');
+
 class Contenedor{
     constructor(fileData){
         this.fileData = fileData;
         this.countId = 0;
     }
     
-
     //Metodos de la clase
-    save = ({title,price,thumbnail}) => {
+    save = async ({title,price,thumbnail}) => {         
+        try{
+            const informacion = JSON.parse(await fs.promises.readFile('./productos.txt','utf-8')) 
+            if(informacion.length == 0 || informacion.length > 0){
+                informacion.push(
+                    {
+                        title: title,
+                        price: price,
+                        thumbnail: thumbnail,
+                        id: informacion.length + 1
+                    }
+                )
+                await fs.promises.writeFile('./productos.txt',JSON.stringify(informacion, null,2))              
+            }
+
+
+           //console.log(longitud)
+           //const identificador = informacion[0].id
+           //console.log(identificador)
+           
+        }catch(err){
+            console.log('NO SE PUEDE LEER EL ARCHIVO',err)
+        }
+        /*
         let myObj = new Object();
         myObj.title = title;
         myObj.price = price;
         myObj.thumbnail = thumbnail;
         this.countId = this.countId+1;
         myObj.id = this.countId   
-        return `${myObj.id} ${this.fileData}`       
+        return `${myObj.id} ${this.fileData}` 
+        */      
     }
 }
 
 let productos = new Contenedor('./productos.txt');
-console.log(productos.save({title: 'Escuadra',price: 123.45,thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png'}))
-console.log(productos.save({title: 'Calculadora',price: 234.56,thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/calculator-math-tool-school-256.png'}))
-console.log(productos.save({title: 'Globo Terráqueo',price: 345.67,thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/globe-earth-geograhy-planet-school-256.png'}))
+//productos.save({title: 'Escuadra',price: 123.45,thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png'})
+//productos.save({title: 'Calculadora',price: 234.56,thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/calculator-math-tool-school-256.png'})
+productos.save({title: 'Globo Terráqueo',price: 345.67,thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/globe-earth-geograhy-planet-school-256.png'})
