7d54bdeb7d812aa5c0cd02058c8209b40443ca33
Carlos Mariano Belmonte <cbelmonte.laboral@gmail.com>
2022-08-06 12:00:15 -0300

    Se muestra solo el nombre de los productos disponibles en el servidor

--- desafio_ServidorExpress/servidor.js
+++ desafio_ServidorExpress/servidor.js
@@ -10,18 +10,14 @@ const server = app.listen(PORT,() => {
     console.log(`Server Express escuchando en el puerto ${PORT}`)
 })
 
-
-
-
 const main = async() => {
     let objProd = await productos.getAll();
+    let onlyProd = objProd.map(producto => producto.title);
 
     app.get('/productos', (req, res) => { 
-        res.send(objProd)
+        res.send(onlyProd)
     })
-
 }
-
 
 main()
 
