7cb94676ef12dbfa6b1da2dff8ef27349d123e3a
Carlos Mariano Belmonte <cbelmonte.laboral@gmail.com>
2022-08-06 11:54:42 -0300

    Se muestra el array de objetos en el servidor

--- desafio_ServidorExpress/servidor.js
+++ desafio_ServidorExpress/servidor.js
@@ -1,16 +1,28 @@
+const express = require('express')
 const {Contenedor} = require('./contenedor');
 const productos = new Contenedor('./productos.txt');
 
-const main = async () => {
-    //Guardar productos
-    let idA = await productos.save({title: 'Escuadra',price: 123.45,thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png'})
-    let idB = await productos.save({title: 'Calculadora',price: 234.56,thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/calculator-math-tool-school-256.png'})
-    let idC =await productos.save({title: 'Globo Terráqueo',price: 345.67,thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/globe-earth-geograhy-planet-school-256.png'})
-    console.log(`Estos son todos los productos insertados id: \n ${idA} id: ${idB} id: ${idC}`);
+const app = express()
 
-    //Mostramos todos los productos
-    let allProducts = await productos.getAll();
-    console.log('Mostramos todos los productos guardados: \n',allProducts);
+const PORT = process.env.PORT || 8080
+
+const server = app.listen(PORT,() => {
+    console.log(`Server Express escuchando en el puerto ${PORT}`)
+})
+
+
+
+
+const main = async() => {
+    let objProd = await productos.getAll();
+
+    app.get('/productos', (req, res) => { 
+        res.send(objProd)
+    })
+
 }
 
-main();
+
+main()
+
+
