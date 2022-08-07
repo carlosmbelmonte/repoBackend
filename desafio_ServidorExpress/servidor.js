2a653da70cd4c4580db6d130aff714ef49b07346
Carlos Mariano Belmonte <cbelmonte.laboral@gmail.com>
2022-08-07 10:19:11 -0300

    genero los 2 endpoints /productos y /productoRandom

--- desafio_ServidorExpress/servidor.js
+++ desafio_ServidorExpress/servidor.js
@@ -10,15 +10,24 @@ const server = app.listen(PORT,() => {
     console.log(`Server Express escuchando en el puerto ${PORT}`)
 })
 
-const main = async() => {
-    let objProd = await productos.getAll();
-    let onlyProd = objProd.map(producto => producto.title);
-
-    app.get('/productos', (req, res) => { 
-        res.send(onlyProd)
-    })
+const random = (min, max) => {
+    return Math.floor((Math.random() * (max - min + 1)) + min);
 }
 
-main()
+app.get('/productos', async (req, res) => { 
+    let objProd = await productos.getAll();//Traigo el array de objetos
+    let onlyProd = objProd.map(producto => producto.title); //Coloco en el array solo los nombres de los productos
+    res.send(onlyProd)
+})
+app.get('/productoRandom', async (req, res) => { 
+    let objProd = await productos.getAll();
+    let minimo = Math.min(...objProd.map(x=>parseInt(x.id)));//Busco el minimo ID en el array de objetos
+    let maximo = Math.max(...objProd.map(x=>parseInt(x.id)));//Busco el maximo ID en el array de objetos
+    let prodRandom = await productos.getById(random(minimo,maximo))//Traigo el producto que coincide con ese ID random
+    res.send(prodRandom)
+})
+
+
+
 
 
