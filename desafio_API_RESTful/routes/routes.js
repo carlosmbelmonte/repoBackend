211b9d905fce223a094cdff64c428d8f3f461924
Carlos Mariano Belmonte <cbelmonte.laboral@gmail.com>
2022-08-15 13:10:31 -0300

    Se agrega GET /api/productos y GET /api/productos/:id

--- desafio_API_RESTful/routes/routes.js
+++ desafio_API_RESTful/routes/routes.js
@@ -1,8 +1,38 @@
 const { Router } = require('express')
 const router = Router()
 
-router.get('/', (req, res) => {  // GET '/api/productos' -> devuelve todos los productos.
-    res.send(`hola mundo desde rutas`)
+const productos = [{
+    title:"helado",
+    price:123,
+    thumbnail:"1www",
+    id:1
+},{
+    title:"te",
+    price:234,
+    thumbnail:"2www",
+    id:2
+}]
+
+const getById = (x) =>{
+    const idProducto = productos.find(producto => producto.id === x);
+    return idProducto    
+}
+
+router.get('/', (req, res) => {  // Devuelve todos los productos.
+    if(productos.length === 0){
+        res.send({ "error" : "No existen productos" })    
+    }else{
+        res.send({ ...productos } )    
+    }   
+})
+
+router.get('/:id', (req, res) => {
+    let iD = getById(parseInt(req.params.id))
+    if (iD) {
+        res.send(iD)
+    } else {
+        res.send({ error : "Producto no encontrado" });
+    }   
 })
 
 module.exports = router
