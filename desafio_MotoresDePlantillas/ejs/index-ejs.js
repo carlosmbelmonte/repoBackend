2df8d9a6a5a69ce9a766f5666e66f403f8e2d21f
Carlos Mariano Belmonte <cbelmonte.laboral@gmail.com>
2022-08-21 22:37:39 -0300

    Se agrega la carpeta ejs al desafio

---
+++ desafio_MotoresDePlantillas/ejs/index-ejs.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const { router, productos} = require('./routes/routes')
+
+const app = express()
+const PORT = 8080
+const serverPort = app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`)
+})
+serverPort.on('error', error => console.log(`Error en el puerto del servidor: ${error}`))
+
+app.use(express.json())
+app.use(express.urlencoded({ extended : true }))
+
+app.use('/api/productos', router)
+
+app.set('views','./views')
+app.set('view engine','ejs')
+
+app.get('/', (req, res) => {
+    res.render('formulario')
+})
+
+app.get('/productos',(req,res) => {
+    res.render('tabla',{listExist: productos})
+})
