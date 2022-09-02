50be26a3873d37e07b099ea33fb3ae5d381b7c47
Carlos Mariano Belmonte <cbelmonte.laboral@gmail.com>
2022-09-01 21:07:49 -0300

    Se agrega la funcion POST para agrega productos por ID a un carrito con ID

--- Proyecto_Final/PrimeraEntrega/routes/routeCarrito.js
+++ Proyecto_Final/PrimeraEntrega/routes/routeCarrito.js
@@ -2,6 +2,7 @@ const { Router } = require('express')
 const { Contenedor } = require('../public/js/contenedor')
 const routerCarrito = Router()
 const carrito = new Contenedor('./public/carrito.txt')
+const productos = new Contenedor('./public/productos.txt')
 let administrador = true
 
 routerCarrito.post('/', async(req, res) => { //Recibe y agrega un carrito, y devuelve su id asignado
@@ -32,6 +33,38 @@ routerCarrito.delete('/:id', async(req, res) => { //Elimina un carrito según su
     }   
 })
 
+routerCarrito.get('/:id/productos', async(req, res) => { //Devuelve los productos de un carrito segun su id
+    let allCarrito = await carrito.getAll()
+    const iD = allCarrito.find(cart => cart.id === parseInt(req.params.id))
+    if (iD) {
+        res.send(iD.productos)
+    } else {
+        res.status(400).json({ error : "Carrito no encontrado" });
+    }   
+})
 
+routerCarrito.post('/:id/productos', async(req, res) => { //Incorporar productos al carrito por su id de producto
+    let allCarrito = await carrito.getAll()
+    
+    const iDCart = allCarrito.find(cart => cart.id === parseInt(req.params.id))
+    const indexCart = allCarrito.map(cart => cart.id).indexOf(parseInt(req.params.id))
+
+    if (iDCart) {
+        const { idProducto } = req.body
+        let allProductos = await productos.getAll()
+        const iD = allProductos.find(producto => producto.id === idProducto)
+
+        if (iD) {
+            allCarrito[indexCart].productos.push(iD)
+            await carrito.saveAll(allCarrito)
+            res.send(`Producto con ID: ${idProducto} agregado exitosamente al carrito con ID: ${req.params.id}`)    
+        }else{
+            res.status(400).json({ "error": "Ingrese el ID del producto" })
+        }
+
+    }else{
+        res.status(400).json({ error : "Carrito no encontrado" });
+    }   
+})
 
 module.exports = routerCarrito
