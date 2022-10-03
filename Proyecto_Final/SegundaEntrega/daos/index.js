beea796cd8ffff6960032f5fa6b0851b4fd01b31
Carlos Mariano Belmonte <cbelmonte.laboral@gmail.com>
2022-10-03 14:56:33 -0300

    Se agrega el archivo switchDB el cual tiene una variable que permite verificar las diferentes DB

--- Proyecto_Final/SegundaEntrega/daos/index.js
+++ Proyecto_Final/SegundaEntrega/daos/index.js
@@ -1,7 +1,8 @@
+import { varSwitch } from '../switchDB.js'
 let productosDao
 let carritosDao
 
-switch (process.env.PERS) {
+switch (varSwitch) {
     case 'txt':
         const { default: ProductosDaoArchivo } = await import('./productos/ProductosDaoArchivos.js')
         const { default: CarritosDaoArchivo } = await import('./carritos/CarritosDaoArchivo.js')
@@ -15,29 +16,16 @@ switch (process.env.PERS) {
 
         productosDao = new ProductosDaoFirebase()
         carritosDao = new CarritosDaoFirebase()
-        break/*
-    case 'mongodb':
-        const { default: ProductosDaoMongoDb } = await import('./productos/ProductosDaoMongoDb.js')
-        const { default: CarritosDaoMongoDb } = await import('./carritos/CarritosDaoMongoDb.js')
-
-        productosDao = new ProductosDaoMongoDb()
-        carritosDao = new CarritosDaoMongoDb()
-        break*/
-    default:
-        /*const { default: ProductosDaoArchivo } = await import('./productos/ProductosDaoArchivos.js')
-        const { default: CarritosDaoArchivo } = await import('./carritos/CarritosDaoArchivo.js')
-        productosDao = new ProductosDaoArchivo()
-        carritosDao = new CarritosDaoArchivo()
-
-        const { default: ProductosDaoFirebase } = await import('./productos/ProductosDaoFirebase.js')
-        const { default: CarritosDaoFirebase } = await import('./carritos/CarritosDaoFirebase.js')
-        productosDao = new ProductosDaoFirebase()
-        carritosDao = new CarritosDaoFirebase()*/  
+        break
+    case 'mongo':
         const { default: ProductosDaoMongo } = await import('./productos/ProductosDaoMongo.js')
         const { default: CarritosDaoMongo } = await import('./carritos/CarritosDaoMongo.js')
 
         productosDao = new ProductosDaoMongo()
-        carritosDao = new CarritosDaoMongo()              
+        carritosDao = new CarritosDaoMongo()
+        break
+    default: 
+        // do nothing;           
         break
 }
 
---
+++ Proyecto_Final/SegundaEntrega/switchDB.js
@@ -0,0 +1,5 @@
+
+let arrayDB = ['txt','firebase','mongo'] 
+let varSwitch = arrayDB[2]//--- CAMBIAR PARA PROBAR LAS DIFERENTES DB
+
+export { varSwitch }
