b4a826ee2bd753d2019b1014a1b1f70be6e9f275
Carlos Mariano Belmonte <cbelmonte.laboral@gmail.com>
2022-10-12 10:53:52 -0300

    Se crea la carpeta LogIn en base al desafio anterior

---
+++ desafio_LogIn/README.md
@@ -0,0 +1,25 @@
+# Consigna
+Continuando con el desafío de la clase anterior, vamos a incorporar un mecanismo sencillo que permite loguear un cliente por su nombre, mediante un formulario de ingreso.
+Luego de que el usuario esté logueado, se mostrará sobre el contenido del sitio un cartel con el mensaje “Bienvenido” y el nombre de usuario. Este cartel tendrá un botón de deslogueo a su derecha.
+Verificar que el cliente permanezca logueado en los reinicios de la página, mientras no expire el tiempo de inactividad de un minuto, que se recargará con cada request. En caso de alcanzarse ese tiempo, el próximo request de usuario nos llevará al formulario de login.
+Al desloguearse, se mostrará una vista con el mensaje de 'Hasta luego' más el nombre y se retornará automáticamente, luego de dos segundos, a la vista de login de usuario.
+
+
+# Detalles del entregable: 
+- La solución entregada deberá persistir las sesiones de usuario en Mongo Atlas.
+- Verificar que en los reinicios del servidor, no se pierdan las sesiones activas de los clientes.
+- Mediante el cliente web de Mongo Atlas, revisar los id de sesión correspondientes a cada cliente y sus datos.
+- Borrar una sesión de cliente en la base y comprobar que en el próximo request al usuario se le presente la vista de login.
+- Fijar un tiempo de expiración de sesión de 10 minutos recargable con cada visita del cliente al sitio y verificar que si pasa ese tiempo de inactividad el cliente quede deslogueado.
+
+# Dependencias a instalar:
+- npm install express
+- npm install --save-dev nodemon
+- npm install express-handlebars
+- npm install express socket.io
+- npm install @faker-js/faker --save-dev
+- npm install normalizr
+
+# Dependecianas especiales
+Dado que daba un error al ejecutarse el fetch, se tuvo que instalar la siguiente dependencia:
+- npm i node-fetch@2.6.7
---
+++ desafio_LogIn/index.js
@@ -0,0 +1,68 @@
+const express = require("express")
+const { Server: HTTPServer} = require('http')
+const { Server: IOServer } = require("socket.io")
+const { engine } = require('express-handlebars')
+//const { router } = require('./routes/routes') 
+const { routerFaker , productosRandom} = require('./routes/routeFaker')// <-----AGREGADO
+
+const postProducto = require('./public/js/postProducto')
+const { normalize, schema } = require("normalizr")//---->Para Normalizr
+
+const { Contenedor } = require('./public/js/contenedor') //---->Para Normalizr
+const chats = new Contenedor('./public/chat.txt')//---->Para Normalizr
+
+const authorSchema = new schema.Entity('authors', {}, { idAttribute: 'id' }) //---->Para Normalizr
+const messageSchema = new schema.Entity('messages', { author: authorSchema }, { idAttribute: '_id' }) //---->Para Normalizr
+
+let productos = productosRandom()
+
+const app = express()
+const http = new HTTPServer(app)
+const io = new IOServer(http)
+
+app.engine('handlebars',engine())
+app.set('view engine','handlebars')
+app.set('views','./views')
+
+app.use(express.json())
+app.use(express.urlencoded({ extended : true }))
+app.use(express.static("public"))
+
+//app.use('/api/productos', router)
+app.use('/api/productos-test', routerFaker)
+
+const PORT = 8080
+const serverPort = http.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`)
+})
+serverPort.on('error', error => console.log(`Error en el puerto del servidor: ${error}`))
+
+
+app.get('/login', (req, res) => {
+    res.render('formulario',{listExist: productos})
+})
+
+io.on("connection", async(socket) => {
+    console.log("Nuevo cliente conectado")
+    socket.emit('allProductos', productos)
+    let allChats = await chats.getAll()
+    const normalizedMessages = normalize(allChats, [messageSchema])//---->Para Normalizr
+    console.log("Array con todos los chats: ", normalizedMessages)
+    socket.emit('allMensajes', normalizedMessages)
+
+    socket.on('newProducto', async data => {
+        console.log("Nuevo producto agregado: ", data)
+        await postProducto(data)
+        console.log("Array con todos los productos: ", productos)
+        io.sockets.emit('allProductos', productos)
+    })
+
+    socket.on('newMensaje', async msg => {
+        console.log("Nuevo mensaje agregado: ", msg)
+        await chats.saveNormalizr(msg)
+        let newAllChats = await chats.getAll()
+        const newNormalizedMessages = normalize(newAllChats, [messageSchema])//---->Para Normalizr
+        console.log("Array con todos los chats: ", newNormalizedMessages)
+        io.sockets.emit('allMensajes', newNormalizedMessages)
+    })
+})
