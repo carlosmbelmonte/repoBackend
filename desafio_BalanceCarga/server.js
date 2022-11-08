8d950a656152f7bda8c4fa3c7f57b9e6cf365f18
Carlos Mariano Belmonte <cbelmonte.laboral@gmail.com>
2022-11-08 20:49:04 -0300

    Se agrega diferenciacion para NGINX

--- desafio_BalanceCarga/server.js
+++ desafio_BalanceCarga/server.js
@@ -186,37 +186,8 @@ if(modoCluster && cluster.isPrimary) {
       cluster.fork()
   })
 }
-else {
-  /*const port = parseInt(process.argv[2]) || parseInt(process.env.PUERTO)
-
-  controllersdb.conectarDB(process.env.URLBASE,JSON.parse(process.env.MI_USER),err => {
-    if (err) return console.log('error en conexión de base de datos', err);
-    console.log('BASE DE DATOS CONECTADA');
-  
-    http.listen(port, (err) => {
-      if (err) return console.log('error en listen server', err);
-      console.log(`Servidor express escuchando en el puerto ${port} - PID WORKER ${process.pid}`)
-    })  
-  })*/
-
-  if(parseInt(process.argv[2])===8081){
-    const appRandom = express()
-    appRandom.use('/api/randoms', router)
-    appRandom.listen(8081, err => {
-        if(!err) console.log(`Servidor express escuchando en el puerto ${parseInt(process.argv[2])} - PID WORKER ${process.pid}`)
-    })            
-  }else{
-    const port = parseInt(process.env.PUERTO)
-
-    controllersdb.conectarDB(process.env.URLBASE,JSON.parse(process.env.MI_USER),err => {
-      if (err) return console.log('error en conexión de base de datos', err);
-      console.log('BASE DE DATOS CONECTADA');
-    
-      http.listen(port, (err) => {
-        if (err) return console.log('error en listen server', err);
-        console.log(`Servidor express escuchando en el puerto ${port} - PID WORKER ${process.pid}`)
-      })  
-    })}  
+else {  
+  estadoNgix(process.argv[4])
 }
 
 // ------------------------------------------------------------------------------
@@ -239,3 +210,43 @@ io.on("connection", async(socket) => {
       io.sockets.emit('allMensajes', newNormalizedMessages)
   })
 })
+
+function estadoNgix(varNginx){
+  if(varNginx === 'SINIGNX'){
+    if(parseInt(process.argv[2])!==8080){
+      const appRandom = express()
+      appRandom.use('/api/randoms', router)
+      appRandom.listen(parseInt(process.argv[2]), err => {
+          if(!err) console.log(`Servidor express escuchando en el puerto ${parseInt(process.argv[2])} - PID WORKER ${process.pid}`)
+      })  
+      appRandom.get('/api/randoms', (req,res) => {
+        res.send(`Server en PORT(${parseInt(process.argv[2])}) - PID(${process.pid}) - FYH(${new Date().toLocaleString()})`)
+      })          
+    }else{
+      const port = parseInt(process.env.PUERTO)
+  
+      controllersdb.conectarDB(process.env.URLBASE,JSON.parse(process.env.MI_USER),err => {
+        if (err) return console.log('error en conexión de base de datos', err);
+        console.log('BASE DE DATOS CONECTADA');
+      
+        http.listen(port, (err) => {
+          if (err) return console.log('error en listen server', err);
+          console.log(`Servidor express escuchando en el puerto ${port} - PID WORKER ${process.pid}`)
+        })  
+      })
+    }
+  }
+  if(varNginx!=='SINIGNX'){
+    const port = parseInt(process.argv[2]) || parseInt(process.env.PUERTO)
+
+    controllersdb.conectarDB(process.env.URLBASE,JSON.parse(process.env.MI_USER),err => {
+      if (err) return console.log('error en conexión de base de datos', err);
+      console.log('BASE DE DATOS CONECTADA');
+    
+      http.listen(port, (err) => {
+        if (err) return console.log('error en listen server', err);
+        console.log(`Servidor express escuchando en el puerto ${port} - PID WORKER ${process.pid}`)
+      })  
+    })    
+  }
+}
