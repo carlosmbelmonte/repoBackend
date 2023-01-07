39dc4404911b06878ac7763ddbfeb35bbcd7645f
Carlos Mariano Belmonte <cbelmonte.laboral@gmail.com>
2023-01-07 09:56:59 -0300

    Se hace un test manual de axios

--- desafio21_TestApi/CLIENTE/clienteAxios.js
+++ desafio21_TestApi/CLIENTE/clienteAxios.js
@@ -35,29 +35,6 @@ async function put(x,obj) {
     }
 }
 
-
-const main = async() => {
-    console.log(">>>> Mostrar todos los productos <<<<")
-    const getProductos = await get()
-    console.log(getProductos)
-
-    console.log(">>>> Agregar un producto <<<<")
-    const postProductos = await post({"nombre": "Carlos Belmonte","descripcion": "Medidor Persona","codigo": "29101990","foto": "https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/batman_hero_avatar_comics-64.png","precio": "32 usd","stock": "1 unidades"})
-    console.log(postProductos)
-
-    console.log(">>>> Actualizar un producto <<<<")
-    const xproducts = await get()
-    const putProductos = await put(parseInt(xproducts[xproducts.length - 1].id),{"nombre": "Carlos Mariano Belmonte","descripcion": "Apellido Persona","codigo": "29101990","foto": "https://cdn0.iconfinder.com/data/icons/kameleon-free-pack-rounded/110/Batman-64.png","precio": "32 usd","stock": "1 unidades"})
-    console.log(putProductos)
-    
-    console.log(">>>> Eliminar un producto <<<<")
-    const yproducts = await get()
-    const deleteProducto = await deleteId(parseInt(yproducts[yproducts.length - 1].id))
-    console.log(deleteProducto)
-}
-
-main()
-
 module.exports = {
     get,
     post,
